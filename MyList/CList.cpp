0bd0b6c58840ab30b060f5a9a4aa98d7e70672f6
Jimin Yun <127750424+jjmmnd@users.noreply.github.com>
2025-02-20 18:04:07 +0900

    MyList

---
+++ MyList/CList.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+#include <cstdlib>
+#include "CList.h"
+
+using namespace std;
+
+int main()
+{
+	CList<int> list;
+	list.push_back(1);
+	list.push_back(2);
+	list.push_back(3);
+
+	for (auto a = list.begin(); a != list.end(); ++a)
+	{
+		cout << *a;
+	}
+
+//	cout << *list.begin();
+
+	return 0;
+}
